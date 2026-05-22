a336b51fac8829612ff99ce4cb530ae43115bfff
Júlia <lia.carvalhodacosta@gmail.com>
2026-05-22 09:23:33 -0300

    criando função hash

--- src/hash.c
+++ src/hash.c
@@ -3,3 +3,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define TAMANHO_HASH 97
+
+int calcularHash(char *chave){
+    int hash = 0; 
+
+    while(*chave){
+        hash = (hash * 31 + *chave) % TAMANHO_HASH;
+        chave++;
+    }
+
+        return hash;
+}
+
+void inicializarTabela(HashNode *tabela[]){
+    for (int i=0; i< TAMANHO_HASH ; i++){
+        tabela[i]= NULL;
+    }
+}
