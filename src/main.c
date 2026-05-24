5d69fe7e1ee0e09c114d1af7ff1acfb51b9bf001
Júlia <lia.carvalhodacosta@gmail.com>
2026-05-24 15:39:24 -0300

    interface

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@
 #include "chef.h"
 
 #define TAMANHO_HASH 97
+
 void imprimirResultados(ListaReceitas* lista){
     if (lista == NULL || lista->tamanho == 0) {
         printf("Nenhum prato encontrado com esses criterios.\n");
@@ -245,74 +246,4 @@ do {
     } while (opcaoInicial != 0);
 
     return 0;
-}
-
-void modoBusca(ArvoreTrie* trieNomes, ArvoreIngredientes* trieIngredientes){
-    int opcao = -1;
-    char busca[100];
-
-    while(opcao != 0){
-        printf("MODO DE BUSCA\n");
-        printf("Escolha uma opção:\n");
-        printf("1- Buscar por ingrediente\n");
-        printf("2-Buscar Receitas pelo nome\n");
-        printf("0- Voltar ao menu principal\n");
-        scanf("%d", &opcao);
-
-    getchar();
-
-    if(opcao == 1){
-        printf("Digite o ingrediente ou o prefixo:\n");
-        fgets(busca, sizeof(busca), stdin);
-        busca[strcspn(busca, "\n")] = 0;
-        
-        ListaReceitas* resultados = buscarPrefixo(trieIngredientes, busca);
-        imprimirResultados(resultados);
-    }
-    else if(opcao == 2){
-        printf("Digite o nome ou prefixo da receita: \n");
-        fgets(busca, sizeof(busca), stdin);
-        busca[strcspn(busca, "\n")] == 0;
-
-        ListaReceitas* resultados = buscarPrefixo(trieNomes, busca);
-        imprimirResultados(resultados);
-    }
-    else if(opcao != 0){
-        printf("opção invalida\n");
-        }
-    }
-}
-
-void modoChef(Receita** catalogo, int totalReceitas){
-    int opcao = -1;
-
-    while(opcao != 0){
-        printf("MODO CHEF\n");
-        printf("Escolha uma opção:\n");
-        printf("1- Menu Econõmico\n");
-        printf("2-Menu Rápido\n");
-        printf("0- Voltar ao menu principal\n");
-        scanf("%d", &opcao);
-
-        if(opcao == 1){
-            double orcamento;
-            printf("Qual o orçamento máximo?\n");
-            scanf("%lf", &orcamento);
-
-            ListaReceitas* sugestoes = gerarEconomico(catalogo, totalReceitas, orcamento);
-            imprimirResultados(sugestoes);
-        } 
-        else if( opcao == 2){
-            int tempo;
-            printf("Qual o tempo máximo disponível?(minutos)\n");
-            scanf("%d", &tempo);
-
-            ListaReceitas* sugestoes = gerarRapido(catalogo, totalReceitas, tempo);
-            imprimirResultados(sugestoes);
-        }
-        else if(opcao != 0){
-            printf("Opção inválida\n");
-        }
-
-    }
 }
